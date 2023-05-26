1d62221a52e5166bf62f954998c904fb6d9dc774
Sabrina Inzillo <petitcomiteparis@gmail.com>
2023-05-26 09:46:00 +0200

    Initial Commit

---
+++ ChatAppDemo1/ChatAppDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemo1App.swift
+//  ChatAppDemo1
+//
+//  Created by Sabrina Inzillo on 26.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
